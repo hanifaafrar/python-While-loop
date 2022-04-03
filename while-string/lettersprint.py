cb5f11047e0039b60cd3e459cd4c3a03258cbfff
afrar <57473343+hanifaafrar@users.noreply.github.com>
2022-04-03 23:51:17 +0530

    printing the once and break the code

--- while-string/lettersprint.py
+++ while-string/lettersprint.py
@@ -2,6 +2,7 @@ name = input('Enter Name: \n')
 name1 = len(name)
 index = 0
 while index < name1:
-    if name[index] in ['a', 'e', 'i', 'o', 'u']:
-        print(name[index] , end=' ')
+    if name[index]  in 'a':
+        print(name , end=' ')
+        break
     index = index + 1
