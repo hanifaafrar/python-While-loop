977c196a24b619301e24b8691da93ab27e683ce7
afrar <57473343+hanifaafrar@users.noreply.github.com>
2022-04-04 21:31:28 +0530

    print the vowels

--- while-string/lettersprint.py
+++ while-string/lettersprint.py
@@ -2,7 +2,6 @@ name = input('Enter Name: \n')
 name1 = len(name)
 index = 0
 while index < name1:
-    if name[index]  in 'a':
-        print(name , end=' ')
-        break
+    if name[index]  in ['a', 'e', 'i', 'o', 'u']:
+        print(name[index] , end=' ')
     index = index + 1
