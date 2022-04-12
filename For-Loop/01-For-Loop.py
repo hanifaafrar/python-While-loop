0e6dc21734a8a3ac39e146d07838228595a64279
afrar <57473343+hanifaafrar@users.noreply.github.com>
2022-04-12 22:37:10 +0530

    Printing numbers in forwared and reverse order

---
+++ For-Loop/01-For-Loop.py
@@ -0,0 +1,9 @@
+number = int(input('Enter Number: \n'))
+
+#Printing numbers
+for no in range (number):
+    print(no, end =' \n' )
+
+#printing number in reverse order
+for no in range (number, 0 , -1):
+    print(no)
