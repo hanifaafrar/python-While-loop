7dc0536085b3519e5d34712c89fb02c8cbeeb34a
afrar <57473343+hanifaafrar@users.noreply.github.com>
2022-04-12 22:51:16 +0530

    Printing the letetrs of a word

---
+++ For-Loop/03-For-Loop.py
@@ -0,0 +1,11 @@
+name = input('Enter Name: \n')
+#lenname = len(name)
+#index = 0
+#while lenname > index:
+#    print(name[index])
+#    index = index + 1
+
+
+#printing letters of a word
+for na in name:
+    print(na, end=' :')
