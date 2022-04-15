78f749163f22daa6d963a2d9028db264a9b93ec0
afrar <57473343+hanifaafrar@users.noreply.github.com>
2022-04-15 13:50:36 +0530

    #Prime Number with for loop

--- For-Loop/06-For_Loop.py
+++ For-Loop/06-For_Loop.py
@@ -1,4 +1,19 @@
-#Prime Number with for loop
 number = int(input('Enter Number: \n'))
+#div = 2
+
+#while number > div:
+#    if number % div == 0:
+#       print('Not Prime')
+#        break
+#    div = div + 1
+#else:
+#    print('Prime')
 
 
+#Prime Number with for loop
+for no in range(2, number):
+    if number % no == 0:
+        print('Not Prime')
+        break
+else:
+    print('Prime')
